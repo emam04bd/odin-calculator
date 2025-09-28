aeb85fbcd3d64876aa51039ecf2d84b5486bfb90
emam04bd <emam04bd@gmail.com>
2025-09-28 19:01:18 +0600

    feat: enable oparend part

---
+++ script.js
@@ -0,0 +1,18 @@
+const operandEl = document.querySelector("#operand");
+const displayEl = document.querySelector("#display");
+
+operandEl.addEventListener("click", (e) => {
+	const char = e.target.textContent;
+	const text = displayEl.value;
+	if (char.length == 1 && text.length <= 20) {
+		if (char != "." || (char == "." && !text.includes("."))) {
+			displayEl.value = text + char;
+		}
+	} else if (char == "+/-") {
+		if (text[0] != "-") {
+			displayEl.value = "-" + text;
+		} else {
+			displayEl.value = text.slice(1);
+		}
+	}
+});
